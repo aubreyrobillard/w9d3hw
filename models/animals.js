97179d30140abd0fd947b883155cb6ae68316a7f
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-07-01 02:40:27 -0500

    Index page working

---
+++ models/animals.js
@@ -0,0 +1,13 @@
+const mongoose = require('./connection');
+
+const animalSchema = new mongoose.Schema ({
+    species: String,
+    extinct: Boolean,
+    location: String,
+    lifeExpectancy: Number
+})
+
+const AnimalModel = mongoose.model('animal', animalSchema);
+
+module.exports = AnimalModel;
+
